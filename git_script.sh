d81e8e70cf05fd724bc19e6260e5b228eb9d74d7
April Cheng <acheng118@my.bcit.ca>
2023-09-23 14:42:59 -0700

    Update code

--- git_script.sh
+++ git_script.sh
@@ -1,63 +1,246 @@
 #!/bin/bash
 
 mygit-init() {
-    cd $1 # enter directory
-    echo currently in $1 directory
-    echo initializing git init
-    git init
-    echo complete git init in $1 directory
+    echo ===========================================
+    echo mygit-init command called
+
+    directory = $1
+
+    # if number of arguments does 'not equal' to 1
+    if [ $# -ne 1 ]
+    then
+        echo Error: improper usage, try mygit-init "<directory>"
+        exit 1
+    fi
+
+    # if the directory does not exists
+    if [ ! -d "$directory" ]
+    then
+        echo Error: directory does not exists: $directory
+        exit 1
+    fi
+
+    cd $directory || exit 1 # enter directory or fail
+    echo Currently in $directory directory
+    echo Initializing git init
+    git init || {
+        echo "Error: failed to initialize git repository"
+        exit 1
+    }
+    echo COMPLETE git init in $directory directory
 }
 
+
 mygit-clone() {
-    cd $2 # directory
-    echo currently in $2 directory
-    echo cloning remote directory from $1
-    git clone $1 # remote repo
-    echo complete cloning remote directory
+    echo ===========================================
+    echo mygit-clone command called
+
+    remote_url = $1
+    local_directory = $2
+
+    # if number of arguments does 'not equal' to 2
+    if [ $# -ne 2 ]
+    then
+        echo ERROR: improper usage, try mygit-clone "<remote_url> <local_directory>"
+        exit 1
+    fi
+
+    # checks if the directory already exists
+    if [ -d "$local_directory" ]
+    then  
+        echo ERROR: directory already exists: $local_directory
+        exit 1
+    fi
+
+    cd "$(dirname "$local_directory")" || exit 1 # cd into directory or exit
+    echo currently in $(pwd) directory
+    echo cloning remote directory from $remote_url
+    git clone $remote_url || {
+        echo Error: Failed to clone remote repository
+        exit 1
+    } 
+    echo COMPLETE cloning remote directory to local directory
 }
 
 mygit-commit() {
-    # verify that -m is entered
+    echo ===========================================
+    echo mygit-commit command called
+
+    commit_message = $2
+
+    # if number of arguments does 'not equal' to 2. For the -m and commit_message
+    if [ $# -ne 2 ]
+    then
+        echo ERROR: improper usage, try mygit-commit -m "<commit_message>"
+        exit 1
+    fi
+
+    # verify that -m is entered for the first argument
     if [ "$1" != "-m" ]; then
-        echo "Error: incorrect syntax. Use mygit-commit -m "commit_message""
+        echo ERROR: improper usage, try mygit-commit -m "<commit_message>"
         exit 1
     fi
 
     echo adding to git
-    git add .
+    git add . || {
+        echo Error: Failed to add files to git
+        exit 1
+    }
     echo commiting to git
-    git commit -m "$2"
-    echo git commit completed
+    git commit -m "$commit_message" || {
+        echo ERROR: failed to commit changes
+        exit 1
+    }
+    echo COMPLETE git commit
 }
 
 mygit-push() {
+    echo ===========================================
+    echo mygit-push command called
+
+    # check if there is 0 arguments
+    if [ $# -ne 0 ]
+    then
+        echo ERROR: improper usage, try mygit-push
+        exit 1
+    fi
+
     echo pushing changes to remote repo
-    git push
-    echo push to remote repo completed
+    git push || {
+        echo ERROR: failed to push changes to remote repository
+        exit 1
+    }
+    echo COMPLETE push to remote repo
 }
 
 mygit-create-directory() {
-    echo creating new directory $1
-    mkdir $1 # new directory name
-    echo new directory $1 created
+    echo ===========================================
+    echo mygit-create-directory command called
+
+    directory_name = $1
+
+    # check if there is 1 argument
+    if [ $# -ne 1 ]
+    then 
+        echo ERROR: improper usage, try mygit-create-directory "<directory_name>"
+        exit 1
+    fi
+
+    # check if the directory exists
+    if [ -d "$directory_name" ]
+    then
+        echo ERROR: directory already exists $directory_name
+        exit 1
+    fi
+
+    echo creating new directory $directory_name
+    mkdir $directory_name || {
+        echo ERROR: failed to create directory
+        exit 1
+    }
+    echo COMPLETE created new directory $directory_name
 }
 
 mygit-delete-file(){
-    echo removing $1 file from git repo
-    rm $1 # delete file from repo
-    echo file $1 removed from git repo
+    echo ===========================================
+    echo mygit-delete-file command called
+
+    file_name = $1
+
+    # check if there is 1 argument
+    if [ $# -ne 1 ]
+    then 
+        echo ERROR: improper usage, try mygit-delete-file "<file_name>"
+        exit 1
+    fi
+
+    # check if file does not exists
+    if [ ! -f "$file_name" ]
+    then
+        echo ERROR: $file_name does not exists
+        exit 1
+    fi
+
+    echo removing $file_name file from git repo
+    rm $file_name || {
+        echo ERROR: Failed to remove file from git repo
+        exit 1
+    }
+    echo COMPLETE removing file $file_name from git repo
 }
 
 mygit-delete-directory(){
-    echo removing $1 directory from git repo
-    git rm -r $1 # delete directory and its content from repository
-    echo removed $1 directory from git repo
+    echo ===========================================
+    echo mygit-delete-directory command called
+
+    directory_name = $1
+
+    # check if there is 1 argument
+    if [ $# -ne 1 ]
+    then
+        echo ERROR: improper usage, try mygit-delete-directory "<directory_name>"
+        exit 1
+    fi 
+
+    # check if directory exists
+    if [ ! -d "$directory_name" ]
+    then 
+        echo ERROR: directory does not exists
+        exit 1
+    fi
+
+    echo removing $directory_name directory from git repo
+    git rm -r $directory_name || {
+        echo ERROR: failed to remove directory and its contents from git repo
+        exit 1
+    }
+    echo COMPLETE removal of $directory_name directory from git repo
 }
 
 mygit-list-contents(){
+    echo ===========================================
+    echo mygit-list-contents command called
+
+    directory = $1
+
     echo listing all contents:
-    ls $1 
+    ls $directory || {
+        echo ERROR: failed to list directory contents
+        exit 1
+    }
+    echo COMPLETE listing directory contents
 }
+
+menu(){
+    echo ================= MENU =================
+    echo List of available commands: 
+    echo
+    echo mygit-init "<directory>"
+    echo description: initializes a new git repo in the specified directory
+    echo 
+    echo mygit-clone "<remote_url> <local_directory>"
+    echo description: clone an existing git repo from a remote URL to a local directory
+    echo
+    echo mygit-commit "-m <commit_message>"
+    echo description: add and commit changes to the local git repo
+    echo
+    echo mygit-push
+    echo description: push committed changes to remote git repo
+    echo
+    echo mygit-create-directory "<directory_name>"
+    echo description: creates a new directory within the git repo
+    echo
+    echo mygit-delete-file "<file_name>"
+    echo description: deletes a file from the git repo
+    echo
+    echo mygit-delete-directory "<directory_name>"
+    echo description: deletes a directory and its content from the git repo
+    echo
+    echo mygit-list-contents "<directory>"
+    echo description: lists the contents of a directory within the git repo
+    echo ========================================
+}
+
 
 # checks number of CLI arguments
 if [ $# -eq 0 ]; then
@@ -66,6 +249,9 @@ if [ $# -eq 0 ]; then
 fi
 
 case "$1" in 
+    "menu")
+        menu
+        ;;
     "mygit-init")
         mygit-init $2
         ;;
